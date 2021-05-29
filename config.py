a34830150bc33234c4914d4e9918f9423c2c3d00
김음화 <eumhwa@gim-eumhwaui-MacBookAir.local>
2021-05-29 21:55:34 +0900

    update

--- config.py
+++ config.py
@@ -1,21 +1,9 @@
 import argparse
 
-# train parameters
-epoch = 100
-batch_size = 8
-lr = 0.0003
-
-# additional parameters
-chnum_in_ = 1
-mem_dim_in = 2048
-shrink_threshold = 0.0025
-entropy_loss_weight = 0.0002
-
-device = torch.device("cuda")
-ckpt_nm = "memAE_MNIST.pt"
 
 def get_params_mnist():
     parser = argparse.ArgumentParser(description='config parameters for memAE_MNIST.py')
+    
     parser.add_argument('--epoch', type=int, default=100, help='training epochs')
     parser.add_argument('--batch_size', type=int, default=4, help='training mini batch size')
     parser.add_argument('--lr', type=float, default=0.00003, help='training learning rate')
