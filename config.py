2ede58d85da4067d4057cc3290eee1046b598456
eumhwa <dmaghk1007@naver.com>
2021-05-30 17:24:45 +0900

    update default value

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ def get_params_AD():
     parser.add_argument('--epoch', type=int, default=100, help='training epochs')
     parser.add_argument('--batch_size', type=int, default=4, help='training mini batch size')
     parser.add_argument('--lr', type=float, default=0.00003, help='training learning rate')
-    parser.add_argument('--chnum_in', type=int, default=1, help='size of training input channel')
+    parser.add_argument('--chnum_in', type=int, default=3, help='size of training input channel')
     parser.add_argument('--mem_dim_in', type=int, default=512, help='dimension of memory module')
     parser.add_argument('--shrink_threshold', type=float, default=0.0025, help='hard shrinkage parameter for memory modeule')
     parser.add_argument('--entropy_loss_weight', type=float, default=0.0002, help='loss parameter')
