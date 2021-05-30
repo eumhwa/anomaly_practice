11cacf9ee0dcfbe8e062ddb95e1579a72180ab53
김음화 <eumhwa@gim-eumhwaui-MacBookAir.local>
2021-05-30 15:37:00 +0900

    update config

--- config.py
+++ config.py
@@ -15,3 +15,21 @@ def get_params_mnist():
     parser.add_argument('--ckpt_name', type=str, default="memAE_MNIST.pt", help='checkpoint name')
 
     return parser
+
+def get_params_AD():
+    parser = argparse.ArgumentParser(description='config parameters for anomaly detection')
+
+    parser.add_argument('--data_path', type=str, default="./datasets/screw", help='training device')
+    parser.add_argument('--epoch', type=int, default=100, help='training epochs')
+    parser.add_argument('--batch_size', type=int, default=4, help='training mini batch size')
+    parser.add_argument('--lr', type=float, default=0.00003, help='training learning rate')
+    parser.add_argument('--chnum_in', type=int, default=1, help='size of training input channel')
+    parser.add_argument('--mem_dim_in', type=int, default=512, help='dimension of memory module')
+    parser.add_argument('--shrink_threshold', type=float, default=0.0025, help='hard shrinkage parameter for memory modeule')
+    parser.add_argument('--entropy_loss_weight', type=float, default=0.0002, help='loss parameter')
+    parser.add_argument('--device', type=str, default="cpu", help='training device')
+    parser.add_argument('--ckpt_name', type=str, default="memAE_MNIST.pt", help='checkpoint name')
+    parser.add_argument('--nf1', type=int, default=32, help='number of first filters')
+    parser.add_argument('--min_filter_size', type=int, default=2, help='number of desired size of filters at first residual block')
+
+    return parser
